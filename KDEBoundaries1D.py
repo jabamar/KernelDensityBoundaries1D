ec16ba6690cc9cced31b7aba0b4b4f0eded80709
Javier Barrios <jbarriosmarti@km3net.de>
2018-07-06 17:44:02 +0200

    Added reflection boundary condition + code simplifications

--- KDEBoundaries1D.py
+++ KDEBoundaries1D.py
@@ -4,6 +4,30 @@ import numpy as np
 from scipy import stats
 
 KERNELS = ['gaussian']
+BOUNDARY = ['reflection']
+
+
+def GAUSSKERN(xi, mean, bw, npts):
+    """ GAUSSIAN KERNEL
+
+    Parameters
+    __________
+
+    xi: float
+        x range value
+
+    mean: float or list of floats
+        mean values for the gaussian (values of the sample)
+
+    bw: float
+        bandwitdh
+
+    npts: integer
+        number of points
+
+    """
+
+    return np.divide(np.sum(stats.norm.pdf(xi, loc=mean, scale=bw)), npts)
 
 
 class KernelDensityBoundaries1D(BaseEstimator):
@@ -15,7 +39,7 @@ class KernelDensityBoundaries1D(BaseEstimator):
     kernel : string
         kernel to use. Default: gaussian
 
-    boundaries: list of floats
+    range: list of floats
         List of dimension 2 [Xmin, Xmax] which gives the minimum/maximum value
         of the distribution to estimate
 
@@ -23,31 +47,38 @@ class KernelDensityBoundaries1D(BaseEstimator):
         if True, the output of score_samples is given by a spline to make
         the calculations faster
 
+    boundary: string
+        if None, it calculates the KDE ignoring any boundary condition
+        if "reflection", it applies a reflection at both ends of range (see
+        http://www.ton.scphys.kyoto-u.ac.jp/~shino/toolbox/reflectedkernel/reflectedkernel.html)
+
     n_approx : int
         number of points for the spline.
 
 
     """
 
-    def __init__(self, kernel="gaussian", boundaries=None, bandwidth=1,
-                 n_approx=-1):
+    def __init__(self, kernel="gaussian", range=None, bandwidth=1,
+                 boundary=None, n_approx=-1):
         """
         Called when initializing the classifier
         """
         self.kernel = kernel
-        self.boundaries = boundaries
+        self.range = range
         self.n_approx = n_approx
         self.bandwidth = bandwidth
+        self.boundary = boundary
 
         if self.kernel not in KERNELS:
             raise RuntimeError("Kernel not valid!")
 
-        if self.n_approx >= 2 and self.boundaries is None:
+        if self.boundary is not None and self.boundary not in BOUNDARY:
+            raise RuntimeError("Boundary condition not valid!")
+
+        if self.n_approx >= 2 and self.range is None:
             raise RuntimeError("Provide a valid boundary for the spline")
 
         self.do_spline = True if self.n_approx >= 2 else False
-
-        print("Hola")
 
     def fit(self, X, y=None):
         """
@@ -64,14 +95,15 @@ class KernelDensityBoundaries1D(BaseEstimator):
 
         self.Xvalues_ = X.copy()
 
-        if self.boundaries is not None:
+        if self.n_approx >= 2:
 
-            Xmin, Xmax = self.boundaries
+            Xmin, Xmax = self.range
             if Xmin >= Xmax:
                 raise RuntimeError("Xmin must be smaller than Xmax!")
 
             Xvals = np.linspace(Xmin, Xmax, self.n_approx)
-            self.interpol_ = interp1d(Xvals, self.eval(Xvals, False),
+            self.interpol_ = interp1d(Xvals,
+                                      [self.eval(xi, False) for xi in Xvals],
                                       kind="cubic")
 
         return self
@@ -89,14 +121,36 @@ class KernelDensityBoundaries1D(BaseEstimator):
             whether the spline is considered or not
         """
 
+        KERNEL = None
+        samplevalues = self.Xvalues_
+        bw = self.bandwidth
+        npts = self.Xvalues_.shape[0]
+
         if usespline:
             return self.interpol_(xi)
-        else:
+
+        elif self.boundary is None:
             if self.kernel == "gaussian":
-                return np.divide(np.sum(stats.norm.pdf(xi, loc=xcent,
-                                                       scale=self.bandwidth)
-                                        for xcent in self.Xvalues_),
-                                 self.Xvalues_.shape[0])
+                KERNEL = GAUSSKERN
+
+            return KERNEL(xi, samplevalues, bw, npts)
+
+        elif self.boundary == "reflection":
+
+            Xmin, Xmax = self.range
+
+            if self.kernel == "gaussian":
+                KERNEL = GAUSSKERN
+
+            return KERNEL(xi, samplevalues, bw, npts) + \
+                KERNEL(2*Xmin - xi, samplevalues, bw, npts) + \
+                KERNEL(2*Xmax - xi, samplevalues, bw, npts)
+#                return GAUSSKERN(xi, self.Xvalues_, self.bandwidth,
+#                                 self.Xvalues_.shape[0]) + \
+#                       GAUSSKERN(2*Xmin - xi, self.Xvalues_, self.bandwidth,
+#                                 self.Xvalues_.shape[0]) + \
+#                       GAUSSKERN(2*Xmax - xi, self.Xvalues_, self.bandwidth,
+#                                 self.Xvalues_.shape[0])
 
     def score_samples(self, X, y=None):
         """Evaluate the density model on the data.
